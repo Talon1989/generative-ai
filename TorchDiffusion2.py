6f93d4bcacf7831bcc6a31c9d0aaf54660e9f72d
fabio <olivetto1989@mail.com>
2023-10-28 16:47:50 +0200

    .

--- TorchDiffusion2.py
+++ TorchDiffusion2.py
@@ -14,6 +14,11 @@ import matplotlib.pyplot as plt
 from utilities import Swish, display_image_torch
 
 
+'''
+trying to make diffusion model work in pytorch
+'''
+
+
 PATH = '/home/fabio/.kaggle/datasets/pytorch-challange-flower-dataset/dataset/'
 EMA = 999 / 1_000
 EMBEDDED_DIM = 32
@@ -34,11 +39,65 @@ val_dataloader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
 
 images, labels = next(iter(train_dataloader))
-image = images[0].permute((1, 2, 0))
-plt.imshow(image)
-plt.axis('off')
-plt.show()
-plt.clf()
+# image = images[0].permute((1, 2, 0))
+# plt.imshow(image)
+# plt.axis('off')
+# plt.show()
+# plt.clf()
+
+
+def cosine_diffusion_schedule(diffusion_times):
+    if type(diffusion_times) is not torch.Tensor:
+        diffusion_times = torch.Tensor(diffusion_times)
+    noise_rates = torch.sin(diffusion_times * (torch.pi / 2))
+    signal_rates = torch.cos(diffusion_times * (torch.pi / 2))
+    return noise_rates, signal_rates
+
+
+def sinusoidal_embedding(x):
+    start = np.log(1.)
+    end = np.log(1000.)
+    n_samples = EMBEDDED_DIM // 2
+    frequencies = torch.exp(torch.linspace(start, end, n_samples))
+    frequencies = frequencies.unsqueeze(0).unsqueeze(-1).unsqueeze(-1)
+    angular_speeds = frequencies * (2. * torch.pi)
+    embeddings = torch.cat(
+        [torch.sin(angular_speeds * x), torch.cos(angular_speeds * x)],
+        dim=1
+    )
+    return embeddings
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, input_shape: int, width: int):
+        super().__init__()
+        self.width = width
+        self.utility_conv = nn.Conv2d(input_shape, self.width, kernel_size=1)
+        self.conv_1 = nn.Conv2d(input_shape, self.width, kernel_size=3, padding=1)
+        self.conv_2 = nn.Conv2d(self.width, self.width, kernel_size=3, padding=1)
+        self.normalizer = nn.BatchNorm2d(input_shape, affine=False)
+        self.swish = Swish()
+
+    def forward(self, x):
+        n_channels = x.shape[1]
+        if n_channels == self.width:
+            residual = x
+        else:  # increase number of channels
+            residual = self.utility_conv(x)
+        x = self.normalizer(x)
+        x = self.swish(self.conv_1(x))
+        x = self.conv_2(x)
+        return x + residual
+
+
+class DownBlock(nn.Module):
+    pass
+
+
+class UpBlock(nn.Module):
+    pass
+
+
 
 
 
