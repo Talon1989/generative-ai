04e858a481d39e37525f2e495054e66a956c4e0e
fabio <olivetto1989@mail.com>
2023-11-08 11:07:53 +0100

    .

--- TODOTorchEnergy.py
+++ TODOTorchEnergy.py
@@ -1,3 +1,4 @@
+import random
 import numpy as np
 import pandas as pd
 import matplotlib
@@ -15,6 +16,8 @@ from utilities import Swish, display_image_torch
 based on
 https://github.com/phlippe/uvadlc_notebooks/blob/master/docs/tutorial_notebooks/tutorial8/Deep_Energy_Models.ipynb
 '''
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
 transform = transforms.Compose([
@@ -99,6 +102,74 @@ output = cnn(images[0:5])
 # display_image_torch(images[0])
 
 
+# Store the samples of the last couple of batches in a buffer,
+# and re-use those as the starting point of the MCMC algorithm for the next batches.
+# This reduces the sampling cost because the model requires a significantly lower
+# number of steps to converge to reasonable samples. However, to not solely rely on
+# previous samples and allow novel samples as well, we re-initialize 5% of our samples
+# from scratch (random noise between -1 and 1)
+class Sampler:
+    def __init__(self, model, image_shape, sample_size, max_len=2**13):
+        self.model = model  # nn used for modeling e_theta
+        self.image_shape = image_shape
+        self.sample_size = sample_size
+        self.max_len = max_len  # max number of datapoints to keep in the buffer
+        self.examples = [
+            (torch.rand((1, ) + image_shape) * 2 - 1)
+            for _ in range(self.sample_size)
+        ]
 
+    # Sample a new batch of fake images with probability p
+    def sample_new_examples(self, steps:int=60, step_size:int=10, p=5/100):
+        n_new = np.random.binomial(n=self.sample_size, p=p)
+        rand_images = torch.rand((n_new,) + self.image_shape) * 2 - 1
+        old_images = torch.cat(
+            random.choices(self.examples, k=self.sample_size - n_new),
+            dim=0)
+        inp_images = torch.cat([rand_images, old_images], dim=0).detach().to(device)
+        imp_images = Sampler.generate_sample(self.model, inp_images, steps, step_size)
+        self.examples = list(
+            inp_images.to(torch.device('cpu')
+                          ).chunk(chunks=self.sample_size, dim=0)) + self.examples
+        self.examples = self.examples[:self.max_len]
+        return imp_images
 
+    @staticmethod
+    def generate_sample(model:nn.Module, inp_images:torch.Tensor,
+                        n_steps=60, step_size=10, return_img_per_step=False):
+        # before mcmc: since we are only interested in the gradient of the input,
+        # set the model parameters to required_grad = False
+        is_training = model.trainning
+        for p in model.parameters():
+            p.requires_grad = False
+        inp_images.requires_grad = True
+        old_grads_enabled = torch.is_grad_enabled()
+        torch.set_grad_enabled(True)
+        noise = torch.randn(inp_images.shape, device=inp_images.device)
+        images_per_step = []
+        for _ in range(n_steps):
+            # add noise to the input
+            noise.normal_(0, 5/1_000)
+            inp_images.data.add_(noise.data)
+            inp_images.data.clamp_(min=-1., max=1.)
+            # calculate gradients for current input
+            out_images = - model(inp_images)
+            out_images.sum().backward()
+            inp_images.grad.data.clamp_(min=-0.03, max=0.03)  # stabilize and prevent high gradient
+            # apply gradients to current samples
+            inp_images.data.add_(-step_size * inp_images.data)
+            inp_images.grad.detach_()
+            inp_images.grad.zero_()
+            inp_images.data.clamp_(min=-1., max=1.)
+            #
+            if return_img_per_step:
+                images_per_step.append(inp_images.clone().detach())
+        for p in model.parameters():
+            p.requires_grad = True
+        model.train(is_training)
+        torch.set_grad_enabled(old_grads_enabled)
+        if return_img_per_step:
+            return torch.stack(images_per_step, dim=0)
+        else:
+            return inp_images
 
